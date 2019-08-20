df4dc8201497349c0686ca9b09f21248bd1e6bfb
wangzhijing <1144760185@qq.com>
2019-08-20 20:54:08 +0800

    java 后台将base64字符串保存为图片的方法(互转)

---
+++ src/main/java/com/cyh/sfxt/controller/index/test.java
@@ -0,0 +1,12 @@
+package com.cyh.sfxt.controller.index;
+
+
+import java.io.IOException;
+
+public class test {
+        public static void main(String[] args) throws IOException {
+            String imageStr = Base64Utils.GetImageStr("F://img/1.jpg");
+            System.out.println(imageStr);
+            Base64Utils.GenerateImage(imageStr, "F://photos/1.jpg");
+        }
+}
